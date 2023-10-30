2d20fb333ee17ea72d6dc283762a508f6836d744
Gerard Finol <gerard.finol@urv.cat>
2023-10-30 14:07:10 +0100

    Add documentation to AttributeUpdateType
    Signed-off-by: Gerard Finol <gerard.finol@urv.cat>

--- wire_protocol/src/commands.rs
+++ wire_protocol/src/commands.rs
@@ -920,13 +920,43 @@ impl Reply for SegmentAttributeCommand {
 /**
  * 24. UpdateSegmentAttribute Command
  */
+
+//Defines a type of update for a particular Attribute.
 #[derive(Serialize_repr, Deserialize_repr, PartialEq, Debug, Clone)]
 #[repr(u8)]
 pub enum AttributeUpdateType {
+    /**
+     * Only allows setting the value if it has not already been set. If it is set, the update will fail.
+     */
     None = 0,
+
+    /**
+     * Replaces the current value of the attribute with the one given. If no value is currently set, it sets the value
+     * to the given one.
+     */
     Replace = 1,
+    
+    /**
+     * Any updates will replace the current attribute value, but only if the new value is greater than the current
+     * value (or no value defined currently). This does not require the updates to be consecutive. For example,
+     * if A and B (A &lt; B) are updated concurrently, odds are that B will make it but A won't - this will be observed
+     * by either A failing or both succeeding, but in the end, the final result will contain B.
+     */
     ReplaceIfGreater = 2,
+
+    /**
+     * Replaces the current value of the attribute with the sum of the current value and the one given. If no value is
+     * currently set, it sets the given value as the attribute value.
+     */
     Accumulate = 3,
+
+    /**
+     * Any updates will replace the current attribute value, but only if the existing value matches
+     * an expected value. If the value is different the update will fail. This can be used to
+     * perform compare and set operations. The value {io.pravega.segmentstore.contracts.Attributes.NULL_ATTRIBUTE_VALUE}
+     * is used to indicate a non value, i.e., to remove the attribute or if the value is expected not to
+     * exist.
+     */
     ReplaceIfEquals = 4,
 }
 
